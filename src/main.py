36627f041968ad9c1241759be512eb5c22c2619e
Yossi <mynamia@gmail.com>
2022-12-27 13:24:37 +0200

    Vaccinations_scenario_general

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+
 import logging
 import json
 import numpy as np
@@ -101,9 +102,9 @@ def main():
         # "paper_8": paper_8
         #"paper_2_comp_9": paper_2_comp_9
         # "noam_lockdown_scenario": noam_lockdown_scenario
-        # "vaccinations_scenario_general": vaccinations_scenario_general
+         "vaccinations_scenario_general": vaccinations_scenario_general
         # "vaccinations_scenario_households": vaccinations_scenario_households,
-        "Empty_scenario" : Empty_scenario
+        #"Empty_scenario" : Empty_scenario
     }
 
     datas_to_plot = get_datas_to_plot()
